fe2de1cd2c854cc5cf0ab77020fc52dc29b21a48
LegalGoulash <king0234.pro@gmail.com>
2025-04-04 23:19:37 +0200

    holdak elkezdese

--- SolarSystems/Moon.h
+++ SolarSystems/Moon.h
@@ -1,6 +1,30 @@
-#pragma once
+#ifndef MOON_H
+#include <string>
+#include<iostream>
 class Moon
-{private:
+{
+private: 
+	std::string name;
+	double mass;
+	double radius;
+	double distance;
+public:
+	// construktorok
+	Moon(const std::string& name, double mass, double radius, double distance);
 
+	//getterek
+	std::string getName() const;
+	double getMass() const;
+	double getRadius() const;
+	double getDistance() const;
+
+	//setterek
+	void setName(const std::string& newName);
+	void setMass(double newMass);
+	void setRadius(double newRad);
+	void setDistance(double newDist);
 };
+#endif // !MOON_H
+
+
 
