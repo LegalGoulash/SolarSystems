2ae2e4c18a00bdf90194fd35201908354ef59f3b
LegalGoulash <king0234.pro@gmail.com>
2025-04-04 23:41:59 +0200

    hold.cpp

--- SolarSystems/Moon.cpp
+++ SolarSystems/Moon.cpp
@@ -1,1 +1,12 @@
 #include "Moon.h"
+#include <string>
+#include <iostream>
+#include<iomanip>
+#include<stdexcept>
+
+//constructor
+Moon::Moon(const std::string& name, double mass, double radius, double distance)
+	:name(name), mass(mass), radius(radius), distance(distance)
+{
+	if (mass <= 0) throw std::invalid_argument("Legyen pozitiv sullya");
+}
